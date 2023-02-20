ee5d609093041afe84bab5735dbad1c954d8de3f
Ezekiel John <johnezekiel114@yahoo.com>
2023-02-20 10:38:42 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,6 @@ int main(void)
 	else if (n == 0)
 		printf(" %d is zero", n);
 	else
-		printf(" %d is negative", n);
-		printf("/n");
+		printf(" %d is negative", n)
 		
-	return (0);
-}
+	return (0); }
