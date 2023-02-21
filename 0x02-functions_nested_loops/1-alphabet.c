740f566f051f7d5b6d72a35cc951e1a00cf33080
Ezekiel John <johnezekiel114@yahoo.com>
2023-02-21 14:58:03 -0800

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: print alphabets in lowercase 
+ *
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+		_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: Code checks
+ * Return: Always 0.
+ */
+int main(void)
+{	print_alphabet();
+	return (0);
+}
