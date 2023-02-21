fcb6d1cfa3d228603dc79e2c7735dff61eb8a94f
Ezekiel John <johnezekiel114@yahoo.com>
2023-02-21 12:12:52 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ *  main - Entry point
+ *  Description: printing in putchar
+ * _putchar: print the alphabets putchar
+ *  Return: always 0	
+ *
+ */
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('_');
+	return (0);
+}
+
